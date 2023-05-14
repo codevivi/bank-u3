207e9c2ab74733f4e9594a4fe0bdef696e6dc011
Vilma <vilmail@protonmail.com>
2023-05-14 23:14:22 +0300

    added bcrypt methods and users with hashed passes

---
+++ server/src/utils/crypt.js
@@ -0,0 +1,19 @@
+import bcrypt from "bcrypt";
+const saltRounds = 10;
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+};
+//paswords hashed manually
+// hashPassword("Kacius123");
+// hashPassword("Pelius123");
+
+export const comparePassword = async (password, hash) => {
+  try {
+    const res = await bcrypt.compare(password, hash);
+    return res;
+  } catch (e) {
+    return false;
+  }
+};
