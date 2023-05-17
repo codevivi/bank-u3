c56d49a3ee5496d1d0d7a0d756b2628c2b018268
Vilma <vilmail@protonmail.com>
2023-05-17 18:20:33 +0300

    added more methods to models

--- server/src/models/allModels.js
+++ server/src/models/allModels.js
@@ -1,9 +1,29 @@
 import BaseModel from "./BaseModel.js";
 
 const ACCOUNTS_DB_FILE_NAME = "accounts.json";
+const USERS_DB_FILE_NAME = "users.json";
 class AccountsModel extends BaseModel {
   constructor(fileName) {
     super(fileName);
   }
+  async getAllAccountsCount() {
+    let data = await this.getAll(this.path, "utf-8");
+    console.log(data, "get all account count lenght");
+    return data.length;
+  }
+  async getTotalMoneyInAllAccounts() {
+    let accounts = await this.getAll(this.path, "utf-8");
+    return accounts.reduce((acc, curr) => acc + curr.money, 0);
+  }
+}
+class UsersModel extends BaseModel {
+  constructor(fileName) {
+    super(fileName);
+  }
+  async getByEmail(email) {
+    const all = await this.getAll();
+    return all.find((item) => item.email === email);
+  }
 }
 export const accountsModel = new AccountsModel(ACCOUNTS_DB_FILE_NAME);
+export const usersModel = new UsersModel(USERS_DB_FILE_NAME);
