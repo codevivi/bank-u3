b0bd91974ca05994d9b0f2e5637cb66f6bf1fd5d
Vilma <vilmail@protonmail.com>
2023-05-19 00:14:12 +0300

    added dotenv and config to server

---
+++ client/src/utils/config.js
@@ -0,0 +1,1 @@
+export const SERVER_BASE_PATH = "http://localhost:5000/api";
---
+++ server/src/utils/config.js
@@ -0,0 +1,8 @@
+import env from "dotenv";
+env.config();
+
+export const PORT = process.env.PORT;
+export const BASE_PATH = `http://localhost:5000`;
+export const CLIENT = "http://localhost:3000";
+export const SESSION_SECRET = process.env.SESSION_SECRET;
+export const NODE_ENV = process.env.NODE_ENV;
