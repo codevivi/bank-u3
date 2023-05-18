707a5c56478a26e920b3b08db7d55b9419d723c1
Vilma <vilmail@protonmail.com>
2023-05-19 02:19:49 +0300

    to prev commit

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { PORT, CLIENT } from "./src/utils/config.js";
-import { accountsModel, usersModel } from "./src/models/allModels.js";
 import { session, sessionOptions } from "./src/utils/session.js";
-import { comparePassword } from "./src/utils/crypt.js";
+import { authRoute } from "./src/routes/authRoutes.js";
+import { accountsRoute } from "./src/routes/accountsRoutes.js";
+import { stats } from "./src/controllers/statsController.js";
+import { wrongEndPoint } from "./src/middlewares/wrongEndPoint.js";
 
 const app = express();
 
@@ -19,116 +21,11 @@ app.use(session(sessionOptions));
 app.get("/api/", (req, res) => {
   res.send("Server is running");
 });
+app.use("/api/", authRoute);
+app.use("/api/accounts", accountsRoute);
 
-app.post("/api/login", async (req, res) => {
-  if (req.session.isLoggedIn) {
-    res.json({ message: "already logged in" });
-  }
-  try {
-    const { email, password } = req.body;
-    const user = await usersModel.getByEmail(email);
-    if (!user) {
-      res.json({ message: "Did not find user with this email address." });
-      return;
-    }
-    const isMatch = await comparePassword(password, user.password);
-    if (!isMatch) {
-      res.json({ message: "Login details did not match" });
-      return;
-    } else {
-      const userWithoutPassword = { name: user.userName, email: user.email };
-      // res.cookie("bla", "bla");
-      req.session.isLoggedIn = true;
-      req.session.user = userWithoutPassword;
-      res.json({ message: "OK", user: userWithoutPassword });
-    }
-  } catch (e) {
-    console.log(e);
-    res.json({ message: "Internal server error" });
-  }
-});
-
-app.get("/api/accounts", async (req, res) => {
-  try {
-    if (req.session.user) {
-      const accounts = await accountsModel.getAll();
-      res.json({
-        message: "OK",
-        accounts,
-      });
-    } else {
-      res.json({
-        message: "not authentikated",
-      });
-    }
-  } catch (err) {
-    res.json({
-      message: "failure",
-      accounts: null,
-    });
-  }
-});
-
-app.post("/api/accounts", async (req, res) => {
-  try {
-    const id = await accountsModel.add(req.body.account);
-    res.json({
-      message: "OK",
-      promiseId: req.body.promiseId,
-      id,
-    });
-  } catch (err) {
-    res.json({
-      message: "failure",
-      promiseId: req.body.promiseId,
-    });
-  }
-});
-
-app.put("/accounts/:id", async (req, res) => {
-  try {
-    await accountsModel.update(req.params.id, req.body.account);
-    res.json({
-      message: "OK",
-      promiseId: req.body.promiseId,
-      id: req.body.account.id,
-    });
-  } catch (err) {
-    res.json({
-      message: "failure",
-      promiseId: req.body.promiseId,
-      id: req.body.account.id,
-    });
-  }
-});
-
-app.delete("/api/accounts/:id", async (req, res) => {
-  try {
-    await accountsModel.delete(req.params.id);
-    res.json({
-      message: "OK",
-    });
-  } catch (err) {
-    res.json({
-      message: "failure",
-    });
-  }
-});
-
-app.get("/api/stats", async (req, res) => {
-  try {
-    const [count, totalMoney] = await Promise.all([accountsModel.getAllAccountsCount(), accountsModel.getTotalMoneyInAllAccounts()]);
-    res.json({
-      message: "OK",
-      stats: { count: count, totalMoney: totalMoney },
-    });
-  } catch (err) {
-    res.json({
-      message: "failure",
-      stats: null,
-    });
-  }
-});
+app.get("/api/stats", stats);
+app.use(wrongEndPoint);
 
 app.listen(PORT, () => {
   console.log(`Bank U3 server is running on port ${PORT}`);
